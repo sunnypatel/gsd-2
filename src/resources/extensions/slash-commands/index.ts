c944e9522abc027b208e98b945064bfd335efc02
jonathancostin <66714927+jonathancostin@users.noreply.github.com>
2026-03-11 16:45:34 -0500

    feat: add /clear as alias for /new slash command (#59)
    Registers /clear as a command that calls ctx.newSession(), identical to /new.
    Shows in autocomplete when typing /cl with 'Alias for /new' description.

--- src/resources/extensions/slash-commands/index.ts
+++ src/resources/extensions/slash-commands/index.ts
@@ -3,10 +3,12 @@ import createSlashCommand from "./create-slash-command.js";
 import createExtension from "./create-extension.js";
 import auditCommand from "./audit.js";
 import gsdRun from "./gsd-run.js";
+import clearCommand from "./clear.js";
 
 export default function slashCommands(pi: ExtensionAPI) {
   createSlashCommand(pi);
   createExtension(pi);
   auditCommand(pi);
   gsdRun(pi);
+  clearCommand(pi);
 }
